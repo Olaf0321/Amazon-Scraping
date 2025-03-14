b69ac6640206243c2ddf26630b14b0a1fc72a6ee
Julius2Howell <julius2howell321@gmail.com>
2025-03-14 23:29:06 +0900

    feature_go cart page

--- functions/cart_page.py
+++ functions/cart_page.py
@@ -1,14 +1,14 @@
 import random
 
-def human_pause(page):
-    page.wait_for_timeout(random.randint(5000, 10000))
+def human_pause(page, st, ed):
+    page.wait_for_timeout(random.randint(st, ed))
 
 def go_cart_page(page):
     # Wait for the input box to appear before interacting
-    search_box = page.wait_for_selector("input[type='text'][id='twotabsearchtextbox']", timeout=0)
+    search_box_selector = "input[type='text'][id='twotabsearchtextbox']"
+    search_box = page.wait_for_selector(search_box_selector, timeout=0)
 
     # Click on the search box
     search_box.click()
-
-    human_pause(page)
+    human_pause(page, 1000, 2000)
     search_box.press("Enter")
