527962bc37e520411f5cfac2afc21e89948c5884
Julius2Howell <julius2howell321@gmail.com>
2025-03-14 22:58:08 +0900

    feature_go detail page

--- functions/detail_page.py
+++ functions/detail_page.py
@@ -1,7 +1,7 @@
 import random
 
-def human_pause(page):
-    page.wait_for_timeout(random.randint(5000, 10000))
+def human_pause(page, st, ed):
+    page.wait_for_timeout(random.randint(st, ed))
 
 def go_detail_page(page):
     # Wait until all product links are loaded
@@ -9,10 +9,15 @@ def go_detail_page(page):
     page.wait_for_selector(product_selector, timeout=0)
 
     # Scroll randomly like a human
-    for _ in range(random.randint(3, 6)):  
+    scroll_amount_sum = 0
+    for _ in range(random.randint(2, 4)):
         scroll_amount = random.randint(300, 700)
+        scroll_amount_sum += scroll_amount
         page.mouse.wheel(0, scroll_amount)
-        human_pause(page)
+        human_pause(page, 1000, 3000)
+    
+    page.mouse.wheel(0, -scroll_amount_sum)
+    human_pause(page, 1000, 3000)
 
     # Wait for the a link to be visible using class
     links = page.query_selector_all(product_selector)
@@ -20,7 +25,7 @@ def go_detail_page(page):
     # Extract and print href attributes
     hrefs = [link.get_attribute("href") for link in links if link.get_attribute("href")]
 
-    human_pause(page)
+    human_pause(page, 1000, 3000)
     for href in hrefs:
         print(href)
         page.goto(f"https://www.amazon.co.jp{href}", timeout=0)
